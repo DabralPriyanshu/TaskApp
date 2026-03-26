eb27f10af5a1c435a67a1d75c3237fa8b65c5bd5
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-03-26 18:10:51 +0530

    added url

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 export const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL:
+    import.meta.env.VITE_BACKEND_URL ||
+    "https://taskapp-1-004p.onrender.com/api/v1",
   withCredentials: true,
 });
